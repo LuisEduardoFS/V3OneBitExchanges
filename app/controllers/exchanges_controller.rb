1e823157e6a71d5491f5d3f37f9e601e4594dbde
luis Eduardo <luiseduardodudu@hotmail.com>
2020-08-06 15:56:07 -0300

    primeiro comit dos desafios

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -7,4 +7,9 @@ class ExchangesController < ApplicationController
     value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).call
     render json: {"value": value}
   end
+
+  def reverse
+    value = ExchangeService.new(params[:target_currency], params[:source_currency], params[:amount]).call
+    render json: {"value": value}
+  end
 end
